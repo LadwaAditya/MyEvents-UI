cbe81a000a9aaaa56c4a9c262045e163fd7dcc85
Aditya Ladwa <ladwa.aditya@gmail.com>
2018-09-16 16:32:58 +0530

    Feat: Added React router dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {Hello} from "./components/hello";
+import {HashRouter as Router,Route} from "react-router-dom";
 import { EventListContainer } from "./components/event_list_container";
+import {Navigation} from "./components/navigation";
 
 class App extends React.Component<{},{}>{
     render(){
-        return <div className="container">
-        <h1>Myevents</h1>
-        <EventListContainer eventListURL="http://localhost:8181"></EventListContainer>
-    </div>
+        const eventList = () => <EventListContainer eventListURL="http://localhost:8181"></EventListContainer>
+
+        return <Router>
+        <Navigation brandName="MyEvents"/>
+            <div className="container">
+                <h1>Myevents</h1>
+                <Route exact path="/" component={eventList}/>
+            </div>
+        </Router>
     }
 }
 ReactDOM.render(
